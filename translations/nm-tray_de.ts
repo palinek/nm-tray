4f7a8eba968a41adddd832b1c41f575f1ffcd6ee
Peter Mattern <pmattern@arcor.de>
2016-05-05 01:44:38 +0200

    Localization: Add German translation

---
+++ translations/nm-tray_de.ts
@@ -0,0 +1,168 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>ConnectionInfo</name>
+    <message>
+        <location filename="../src/connectioninfo.ui" line="14"/>
+        <source>Connection information</source>
+        <translation>Verbindungsinformationen</translation>
+    </message>
+</context>
+<context>
+    <name>NmList</name>
+    <message>
+        <location filename="../src/nmlist.ui" line="14"/>
+        <source>Dialog</source>
+        <translatorcomment>Did not translate as &quot;Dialog&quot; seems to get replaced with string &quot;nm-tray info&quot; from context &quot;Tray&quot;.</translatorcomment>
+        <translation>Dialog</translation>
+    </message>
+    <message>
+        <location filename="../src/nmlist.ui" line="20"/>
+        <source>All information</source>
+        <translation>Verfügbare Informationen</translation>
+    </message>
+    <message>
+        <location filename="../src/nmlist.ui" line="30"/>
+        <source>Active connections</source>
+        <translation>Aktive Verbindungen</translation>
+    </message>
+    <message>
+        <location filename="../src/nmlist.ui" line="40"/>
+        <source>Available wireless</source>
+        <translation>Verfügbare drahtlose Verbindungen</translation>
+    </message>
+</context>
+<context>
+    <name>NmModel</name>
+    <message>
+        <location filename="../src/nmmodel.cpp" line="745"/>
+        <source>root</source>
+        <translatorcomment>Translated as &quot;network&quot; hence not literally at all. Not sure what&apos;s best here but this seems a bit more reasonable to me.</translatorcomment>
+        <translation>Netzwerk</translation>
+    </message>
+    <message>
+        <location filename="../src/nmmodel.cpp" line="747"/>
+        <source>active connection(s)</source>
+        <translation>Aktive Verbindungen</translation>
+    </message>
+    <message>
+        <location filename="../src/nmmodel.cpp" line="749"/>
+        <source>connection(s)</source>
+        <translation>Verbindungen</translation>
+    </message>
+    <message>
+        <location filename="../src/nmmodel.cpp" line="751"/>
+        <source>device(s)</source>
+        <translation>Geräte</translation>
+    </message>
+    <message>
+        <location filename="../src/nmmodel.cpp" line="753"/>
+        <source>wifi network(s)</source>
+        <translation>Drahtlose Netzwerke</translation>
+    </message>
+    <message>
+        <location filename="../src/nmmodel.cpp" line="914"/>
+        <source>unknown</source>
+        <comment>hardware address</comment>
+        <translation>unbekannt</translation>
+    </message>
+    <message>
+        <location filename="../src/nmmodel.cpp" line="915"/>
+        <source>none</source>
+        <comment>security</comment>
+        <translation>keine</translation>
+    </message>
+    <message>
+        <location filename="../src/nmmodel.cpp" line="948"/>
+        <source>General</source>
+        <comment>Active connection information</comment>
+        <translation>Allgemeines</translation>
+    </message>
+    <message>
+        <location filename="../src/nmmodel.cpp" line="949"/>
+        <source>Interface</source>
+        <comment>Active connection information</comment>
+        <translation>Schnittstelle</translation>
+    </message>
+    <message>
+        <location filename="../src/nmmodel.cpp" line="951"/>
+        <source>Hardware Address</source>
+        <comment>Active connection information</comment>
+        <translation>Hardware-Adresse</translation>
+    </message>
+    <message>
+        <location filename="../src/nmmodel.cpp" line="953"/>
+        <source>Driver</source>
+        <comment>Active connection information</comment>
+        <translation>Treiber</translation>
+    </message>
+    <message>
+        <location filename="../src/nmmodel.cpp" line="955"/>
+        <source>Speed</source>
+        <comment>Active connection information</comment>
+        <translation>Übertragungsrate</translation>
+    </message>
+    <message>
+        <location filename="../src/nmmodel.cpp" line="957"/>
+        <source> Kb/s</source>
+        <translation> Kb/s</translation>
+    </message>
+    <message>
+        <location filename="../src/nmmodel.cpp" line="959"/>
+        <source>unknown</source>
+        <comment>Speed</comment>
+        <translation>unbekannt</translation>
+    </message>
+    <message>
+        <location filename="../src/nmmodel.cpp" line="962"/>
+        <source>Security</source>
+        <comment>Active connection information</comment>
+        <translation>Sicherheit</translation>
+    </message>
+</context>
+<context>
+    <name>Tray</name>
+    <message>
+        <location filename="../src/tray.cpp" line="165"/>
+        <source>Enable Networking</source>
+        <translation>Netzwerk aktivieren</translation>
+    </message>
+    <message>
+        <location filename="../src/tray.cpp" line="166"/>
+        <source>Enable Wi-fi</source>
+        <translation>Drahtlose Verbindungen aktivieren</translation>
+    </message>
+    <message>
+        <location filename="../src/tray.cpp" line="168"/>
+        <source>Connection information</source>
+        <translation>Verbindungsinformationen</translation>
+    </message>
+    <message>
+        <location filename="../src/tray.cpp" line="170"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="../src/tray.cpp" line="172"/>
+        <source>Quit</source>
+        <translation>Beenden</translation>
+    </message>
+    <message>
+        <location filename="../src/tray.cpp" line="226"/>
+        <source>%1 about</source>
+        <translation>%1 über</translation>
+    </message>
+    <message>
+        <location filename="../src/tray.cpp" line="227"/>
+        <source>This is the about nm-tray!</source>
+        <translatorcomment>Pimped this a little bit. Hope that&apos;s okay.</translatorcomment>
+        <translation type="unfinished">nm-tray ist eine auf Qt basierende Nutzeroberfläche für den NetworkManager zur Ausführung in der Leiste von Desktop-Umgebungen.</translation>
+    </message>
+    <message>
+        <location filename="../src/tray.cpp" line="241"/>
+        <source>nm-tray info</source>
+        <translation>nm-tray Informationen</translation>
+    </message>
+</context>
+</TS>
