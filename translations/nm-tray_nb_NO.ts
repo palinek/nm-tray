230cd0cebfc4170d3737d33232f7bbd1111831b3
Allan Nordhøy <epost@anotheragency.no>
2018-12-03 16:21:53 +0000

    Translate using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (52 of 52 strings)
    
    Translation: nm-tray/Translations
    Translate-URL: https://hosted.weblate.org/projects/nm-tray/translations/nb_NO/

--- translations/nm-tray_nb_NO.ts
+++ translations/nm-tray_nb_NO.ts
@@ -236,7 +236,7 @@
     <message>
         <location filename="../src/tray.cpp" line="260"/>
         <source>Wifi - request scan</source>
-        <translation type="unfinished"></translation>
+        <translation>Wi-Fi - forespør skanning</translation>
     </message>
     <message>
         <location filename="../src/tray.cpp" line="261"/>
