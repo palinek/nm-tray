da54fe9b8f076c3de77be0b859c92fcabb393cae
Ács Zoltán <acszoltan111@gmail.com>
2020-01-12 16:33:52 +0000

    Translate using Weblate (Hungarian)
    Currently translated at 70.8% (34 of 48 strings)
    
    Translation: nm-tray/Translations
    Translate-URL: https://hosted.weblate.org/projects/nm-tray/translations/hu/

--- translations/nm-tray_hu.ts
+++ translations/nm-tray_hu.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../src/connectioninfo.ui" line="14"/>
         <source>Connection information</source>
-        <translation type="unfinished">Csatlakozási információ</translation>
+        <translation>Csatlakozási információ</translation>
     </message>
 </context>
 <context>
@@ -19,12 +19,12 @@
     <message>
         <location filename="../src/nmlist.ui" line="27"/>
         <source>Active connections</source>
-        <translation type="unfinished">Aktív kapcsolatok</translation>
+        <translation>Aktív kapcsolatok</translation>
     </message>
     <message>
         <location filename="../src/nmlist.ui" line="37"/>
         <source>Available wireless</source>
-        <translation type="unfinished">Elérhető vezeték nélküli hálózatok</translation>
+        <translation>Elérhető vezeték nélküli hálózatok</translation>
     </message>
 </context>
 <context>
@@ -37,7 +37,7 @@
     <message>
         <location filename="../src/nmmodel.cpp" line="56"/>
         <source>Password is needed for connection to &apos;%1&apos;:</source>
-        <translation type="unfinished">Jelszóra van szükség a (z) &apos; %1 &apos; kapcsolathoz:</translation>
+        <translation>Jelszóra van szükség a (z) &apos; %1 &apos; kapcsolathoz:</translation>
     </message>
     <message>
         <location filename="../src/nmmodel.cpp" line="744"/>
@@ -47,22 +47,22 @@
     <message>
         <location filename="../src/nmmodel.cpp" line="746"/>
         <source>active connection(s)</source>
-        <translation type="unfinished">aktív kapcsolat (ok)</translation>
+        <translation>aktív kapcsolatok</translation>
     </message>
     <message>
         <location filename="../src/nmmodel.cpp" line="748"/>
         <source>connection(s)</source>
-        <translation type="unfinished">kapcsolat (ok)</translation>
+        <translation>kapcsolatok</translation>
     </message>
     <message>
         <location filename="../src/nmmodel.cpp" line="750"/>
         <source>device(s)</source>
-        <translation type="unfinished">eszköz (ök)</translation>
+        <translation>eszközök</translation>
     </message>
     <message>
         <location filename="../src/nmmodel.cpp" line="752"/>
         <source>wifi network(s)</source>
-        <translation type="unfinished">wifi hálózat(ok)</translation>
+        <translation>wifi hálózatok</translation>
     </message>
     <message>
         <location filename="../src/nmmodel.cpp" line="1043"/>
@@ -80,13 +80,13 @@
         <location filename="../src/nmmodel.cpp" line="1152"/>
         <source>Interface</source>
         <comment>Active connection information</comment>
-        <translation type="unfinished">Felület</translation>
+        <translation>Felület</translation>
     </message>
     <message>
         <location filename="../src/nmmodel.cpp" line="1154"/>
         <source>Hardware Address</source>
         <comment>Active connection information</comment>
-        <translation type="unfinished">Hardvercím</translation>
+        <translation>Hardvercím</translation>
     </message>
     <message>
         <location filename="../src/nmmodel.cpp" line="1156"/>
@@ -98,48 +98,48 @@
         <location filename="../src/nmmodel.cpp" line="1158"/>
         <source>Speed</source>
         <comment>Active connection information</comment>
-        <translation type="unfinished">Sebesség</translation>
+        <translation>Sebesség</translation>
     </message>
     <message>
         <location filename="../src/nmmodel.cpp" line="1160"/>
         <source> Kb/s</source>
-        <translation type="unfinished"> Kb/s</translation>
+        <translation> Kb/s</translation>
     </message>
     <message>
         <location filename="../src/nmmodel.cpp" line="1162"/>
         <source>unknown</source>
         <comment>Speed</comment>
-        <translation type="unfinished">ismeretlen</translation>
+        <translation>ismeretlen</translation>
     </message>
     <message>
         <location filename="../src/nmmodel.cpp" line="1166"/>
         <source>Security</source>
         <comment>Active connection information</comment>
-        <translation type="unfinished">Biztonság</translation>
+        <translation>Biztonság</translation>
     </message>
     <message>
         <location filename="../src/nmmodel.cpp" line="1170"/>
         <source>IPv4</source>
         <comment>Active connection information</comment>
-        <translation type="unfinished">IPv4</translation>
+        <translation>IPv4</translation>
     </message>
     <message>
         <location filename="../src/nmmodel.cpp" line="1171"/>
         <source>IPv6</source>
         <comment>Active connection information</comment>
-        <translation type="unfinished">IPv6</translation>
+        <translation>IPv6</translation>
     </message>
     <message>
         <location filename="../src/nmmodel.cpp" line="1182"/>
         <source>IP Address</source>
         <comment>Active connection information</comment>
-        <translation type="unfinished">IP-cím</translation>
+        <translation>IP-cím</translation>
     </message>
     <message>
         <location filename="../src/nmmodel.cpp" line="1184"/>
         <source>Subnet Mask</source>
         <comment>Active connection information</comment>
-        <translation type="unfinished">Alhálózati maszk</translation>
+        <translation>Alhálózati maszk</translation>
     </message>
     <message>
         <location filename="../src/nmmodel.cpp" line="1193"/>
@@ -174,42 +174,42 @@
     <message>
         <location filename="../src/tray.cpp" line="203"/>
         <source>No longer connected to %1 &apos;%2&apos;.</source>
-        <translation type="unfinished">Már nem csatlakozik a (z)% 1 &apos;% 2&apos; -hoz.</translation>
+        <translation>Már nem csatlakozik a(z) %1 &apos;%2&apos; -hoz.</translation>
     </message>
     <message>
         <location filename="../src/tray.cpp" line="215"/>
         <source>Connection established</source>
-        <translation type="unfinished">A kapcsolat létrejött</translation>
+        <translation>A kapcsolat létrejött</translation>
     </message>
     <message>
         <location filename="../src/tray.cpp" line="216"/>
         <source>Now connected to %1 &apos;%2&apos;.</source>
-        <translation type="unfinished">Most kapcsolódott a (z) %1 &apos; %2 &apos; hálózathoz.</translation>
+        <translation>Most kapcsolódott a (z) %1 &apos;%2&apos; hálózathoz.</translation>
     </message>
     <message>
         <location filename="../src/tray.cpp" line="220"/>
         <source>NetworkManager(nm-tray)</source>
-        <translation type="unfinished">Hálózatkezelő (nm-tray)</translation>
+        <translation type="unfinished">Hálózatkezelő</translation>
     </message>
     <message>
         <location filename="../src/tray.cpp" line="253"/>
         <source>Enable Networking</source>
-        <translation type="unfinished">Hálózat engedélyezése</translation>
+        <translation>Hálózatok bekapcsolása</translation>
     </message>
     <message>
         <location filename="../src/tray.cpp" line="254"/>
         <source>Enable Wi-Fi</source>
-        <translation type="unfinished">Wi-Fi engedélyezése</translation>
+        <translation>Wi-Fi bekapcsolása</translation>
     </message>
     <message>
         <location filename="../src/tray.cpp" line="256"/>
         <source>Enable notifications</source>
-        <translation type="unfinished">Értesítések engedélyezése</translation>
+        <translation>Értesítések engedélyezése</translation>
     </message>
     <message>
         <location filename="../src/tray.cpp" line="258"/>
         <source>Connection information</source>
-        <translation type="unfinished">Kapcsolódási információk</translation>
+        <translation>Kapcsolódási információk</translation>
     </message>
     <message>
         <location filename="../src/tray.cpp" line="259"/>
@@ -219,27 +219,27 @@
     <message>
         <location filename="../src/tray.cpp" line="260"/>
         <source>Wifi - request scan</source>
-        <translation type="unfinished">Wifi - keresés</translation>
+        <translation>Wifi - keresés</translation>
     </message>
     <message>
         <location filename="../src/tray.cpp" line="261"/>
         <source>Edit connections...</source>
-        <translation type="unfinished">Kapcsolatok szerkesztése...</translation>
+        <translation>Kapcsolatok szerkesztése...</translation>
     </message>
     <message>
         <location filename="../src/tray.cpp" line="264"/>
         <source>About</source>
-        <translation type="unfinished">Névjegy</translation>
+        <translation>Névjegy</translation>
     </message>
     <message>
         <location filename="../src/tray.cpp" line="266"/>
         <source>Quit</source>
-        <translation type="unfinished">Kilépés</translation>
+        <translation>Kilépés</translation>
     </message>
     <message>
         <location filename="../src/tray.cpp" line="291"/>
         <source>nm-tray info</source>
-        <translation type="unfinished">nm-tray információ</translation>
+        <translation>nm-tray információ</translation>
     </message>
     <message>
         <location filename="../src/tray.cpp" line="384"/>
@@ -249,7 +249,7 @@
     <message>
         <location filename="../src/tray.cpp" line="385"/>
         <source>&lt;strong&gt;&lt;a href=&quot;https://github.com/palinek/nm-tray&quot;&gt;nm-tray&lt;/a&gt;&lt;/strong&gt; is a simple Qt based frontend for &lt;a href=&quot;https://wiki.gnome.org/Projects/NetworkManager&quot;&gt;NetworkManager&lt;/a&gt;.&lt;br/&gt;&lt;br/&gt;Version: %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&lt;strong&gt;&lt;a href=&quot;https://github.com/palinek/nm-tray&quot;&gt;nm-tray&lt;/a&gt;&lt;/strong&gt; egy egyszerű Qt felület a  &lt;a href=&quot;https://wiki.gnome.org/Projects/NetworkManager&quot;&gt;NetworkManager-hez&lt;/a&gt;.&lt;br/&gt;&lt;br/&gt;Version: %1</translation>
     </message>
 </context>
 <context>
@@ -257,17 +257,17 @@
     <message>
         <location filename="../src/windowmenu.cpp" line="163"/>
         <source>Active connection(s)</source>
-        <translation type="unfinished">Aktív kapcsolat(ok)</translation>
+        <translation>Aktív kapcsolatok</translation>
     </message>
     <message>
         <location filename="../src/windowmenu.cpp" line="165"/>
         <source>Wi-Fi network(s)</source>
-        <translation type="unfinished">Wi-Fi hálózat (ok)</translation>
+        <translation>Wi-Fi hálózatok</translation>
     </message>
     <message>
         <location filename="../src/windowmenu.cpp" line="167"/>
         <source>Known connection(s)</source>
-        <translation type="unfinished">Ismert kapcsolat (ok)</translation>
+        <translation>Ismert kapcsolatok</translation>
     </message>
 </context>
 </TS>
